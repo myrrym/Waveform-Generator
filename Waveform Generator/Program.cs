9bbee526756cb9600bc5b8c2def41368472da625
myrrym <miriamho1998@gmail.com>
2023-11-14 10:19:45 +0800

    test

--- Waveform Generator/Program.cs
+++ Waveform Generator/Program.cs
@@ -12,6 +12,7 @@ namespace Waveform_Generator
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
+            // test
         }
     }
 }
