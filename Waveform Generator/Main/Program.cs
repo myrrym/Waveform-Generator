d1c944ec2b97e0f5a6a22eba80d87fd610dddfa5
chunkeat123 <chunkeat123@outlook.com>
2023-11-18 10:27:27 +0800

    Updated from Ai Ning

--- Waveform Generator/Main/Program.cs
+++ Waveform Generator/Main/Program.cs
@@ -16,7 +16,7 @@ namespace Waveform_Generator.Main
         [STAThread]
         static void Main()
         {
-            Application.Run(new Form1());
+            Application.Run(new WaveformGeneratorForm());
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
